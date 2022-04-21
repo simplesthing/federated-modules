13c69c7a5b2967eee6d5b24a6f5b02a3bafcb360
Ava Collins <iamsimplesthing@gmail.com>
2022-04-21 15:15:23 -0700

    remove cra

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,18 +1,27 @@
+const {
+  withStorybookModuleFederation,
+} = require('storybook-module-federation');
 
-
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+module.exports = withStorybookModuleFederation({
+  name: 'components',
+  filename: '__remote/entry.js',
+  exposes: {
+    './Button': require.resolve('../src/components/Button/Button.jsx'),
+  },
+  shared: {
+    react: {
+      singleton: true,
+      requiredVersion: false,
+    },
+    'react-dom': {
+      singleton: true,
+      requiredVersion: false,
+    },
+  },
+})({
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
+});
